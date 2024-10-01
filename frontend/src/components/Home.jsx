f70eda7cb0ab48994c3446cc38f73cb07e815bbb
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-10-01 13:42:10 +0530

    UserContext Working Properly

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,26 @@
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+import { Link } from "react-router-dom";
+
 const Home = () => {
+    const { user } = useContext(UserContext);
     return (
         <div>
             <h1>Home</h1>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+            <Link to="/posts">Posts</Link>
+            <Link to="/posts/new">CreatePost</Link>
+            <Link to="/posts/{id}">Post</Link>
+            {/* Check if user exists and then render the username and email */}
+            {user ? (
+                <p>
+                    <strong>Username:</strong> {user.username}<br />
+                    <strong>Email:</strong> {user.email}
+                </p>
+            ) : (
+                <p>No user information available</p>
+            )}
         </div>
     );
 }
