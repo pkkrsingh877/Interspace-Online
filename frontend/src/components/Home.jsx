eb556f0bae491274b88efa6bdc33227429b7775f
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-10-03 17:29:23 +0530

    Added Favicon and Added bare minimum styles to Homepage for convenience

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,16 +1,19 @@
 import { useContext } from "react";
 import UserContext from "../context/UserContext";
 import { Link } from "react-router-dom";
+import styles from "../css/Home.module.css";
 
 const Home = () => {
     const { user } = useContext(UserContext);
     return (
-        <div>
-            <h1>Home</h1>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-            <Link to="/posts">Posts</Link>
-            <Link to="/posts/new">CreatePost</Link>
+        <div className={styles.main}>
+            <h1>Interspace Online</h1>
+            <div className={styles.links}>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Signup</Link>
+                <Link to="/posts">Posts</Link>
+                <Link to="/posts/new">CreatePost</Link>
+            </div>
             {/* Check if user exists and then render the username and email */}
             {user ? (
                 <p>
